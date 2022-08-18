2be8bd5892a21350f00891914422e6dbcc15f4ef
Vailish <joyksj94@gmail.com>
2022-08-18 17:56:15 +0900

    update

---
+++ 20220812/03_String/05_BOJ_2941_크로아티아_알파벳/s1.py
@@ -0,0 +1,9 @@
+# 크로아티아 알파벳 실버5
+# https://www.acmicpc.net/problem/2941
+
+# c= c- dz= d- lj nj s= z=
+words = input()
+index = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+for idx in index:
+    words = words.replace(idx, '!')  # immutable이므로 대입해줘야함
+print(len(words))
