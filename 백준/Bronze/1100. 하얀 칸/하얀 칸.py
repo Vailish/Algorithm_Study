badde1fb66786d9fb0e8e78a2e76cc7b8ddf4863
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2022-08-22 01:00:53 +0900

    [Bronze II] Title: 하얀 칸, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,9 @@
+chess = [input() for _ in range(8)]
+cnt = 0
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 0 and j % 2 == 0 and chess[i][j] == 'F':  # i, j가 2의 배수일 때
+            cnt += 1
+        if i % 2 == 1 and j % 2 == 1 and chess[i][j] == 'F':  # i, j가 홀 수 일 때
+            cnt += 1
+print(cnt)
