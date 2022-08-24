d58beebdadd858beed61a50bb456b7ac5f0c1687
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2022-08-24 20:24:35 +0900

    [D2] Title: 파리 퇴치, Time: 170 ms, Memory: 61,476 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,18 +1,16 @@
-for case in range(1, 1 + (int(input()))):
-    N, M = map(int, input().split())
+for case in range(1, 1 + int(input())):
+    N, M = map(int, input().split())  # N = 필드, M = 파리채의 크기
+    arr = [list(map(int, input().split())) for _ in range(N)]
 
-    # 파리수 담은 필드 만들기
-    field = [list(map(int, input().split())) for _ in range(N)]
+    kills = []
+    # 파리잡기
+    for i in range(N - M + 1):
+        for j in range(N - M + 1):
+            sum_kill = 0
+            # 파리채의 범위
+            for a in range(i, i + M):
+                for b in range(j, j + M):
+                    sum_kill += arr[a][b]
+            kills.append(sum_kill)
 
-    # 파리채로 잡기
-    kill_sum = 0
-    kill_max = 0
-    for i in range(N-M+1):  # 검사할 i 범위
-        for j in range(N-M+1):   # 검사할 j 범위
-            for num1 in range(i, i + M):   # 검사범위 안의 i
-                for num2 in range(j, j + M):  # 검사범위 안의 j
-                    kill_sum += field[num1][num2]
-            if kill_max < kill_sum:
-                kill_max = kill_sum
-            kill_sum = 0
-    print(f'#{case} {kill_max}')
+    print(f'#{case} {max(kills)}')
