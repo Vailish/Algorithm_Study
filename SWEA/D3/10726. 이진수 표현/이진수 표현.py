4bcd3247b03b9ab8e24d9eb017ee8aa5791c66d2
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2022-09-20 15:30:38 +0900

    [D3] Title: 이진수 표현, Time: 1,280 ms, Memory: 65,444 KB -BaekjoonHub

---
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -0,0 +1,7 @@
+for case in range(1, 1 + int(input())):
+    N, M = map(int, input().split())
+    binary_number = bin(M)[2:]
+    if len(bin(M)[2:]) >= N and '0' not in binary_number[len(binary_number)-N:]:
+        print(f'#{case} ON')
+    else:
+        print(f'#{case} OFF')
