ef691af57bba4a83a409e2b32c660036f60ac50d
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2022-09-22 10:51:33 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+result = 0
+
+for i in range(N - 1, -1, -1):
+    result += K//coins[i]
+    K %= coins[i]
+    if K == 0:
+        break
+
+print(result)
