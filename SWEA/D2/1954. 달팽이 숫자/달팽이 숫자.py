acb0a0a4e4e1d15f727ccdd556b9e6fb0a8f72be
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2022-09-28 14:58:12 +0900

    [D2] Title: 달팽이 숫자, Time: 129 ms, Memory: 58,384 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,31 +1,33 @@
-T = int(input())
-dx = [0, 1, 0, -1]  # 우, 하, 좌, 상 # 방향 꼭꼭 확인하자
-dy = [1, 0, -1, 0]  # 우, 하, 좌, 상
+def dfs(x, y):
+    global cnt, direction
+    cnt += 1
+    visited[x][y] = 1
+    field[x][y] = cnt
 
-for t in range(1, T+1):
+    if field[x][y] == N**2:
+        return
+    # 델타이동
+    nx = x + [0, 1, 0, -1][direction] # 우, 하, 좌, 상
+    ny = y + [1, 0, -1, 0][direction]
+
+    if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] != 1 and field[nx][ny] == 0:
+        dfs(nx, ny)
+    else:
+        direction = (direction + 1) % 4
+        nx = x + [0, 1, 0, -1][direction]
+        ny = y + [1, 0, -1, 0][direction]
+        dfs(nx, ny)
+    return
+
+
+for case in range(1, 1 + int(input())):
     N = int(input())
-    field = [[0]*N for _ in range(N)]
-    x, y = 0, 0  # 달팽이 현재 위치
-    dir = 0 # 우0, 하1, 좌2, 상3
+    field = [[0] * N for _ in range(N)]
+    visited = [[0] * N for _ in range(N)]
+    cnt = 0
+    direction = 0
+    dfs(0, 0)
 
-    # 달팽이전진
-    for num in range(1, N**2 + 1): # 숫자찍기
-        field[x][y] = num
-
-        # 달팽이 이동할 장소 nx, ny
-        nx = x + dx[dir%4]
-        ny = y + dy[dir%4]
-        
-        # 이동 가능 여부 검사
-        if 0 <= nx < N and 0 <= ny < N and field[nx][ny] == 0:
-            x = nx  #실제로 이동
-            y = ny
-        else:  # 못가니까 머리를 돌려놔주자
-            dir = (dir + 1) % 4
-            x += dx[dir]
-            y += dy[dir]
-    
-    # 완주!! 출력!
-    print(f'#{t}')
+    print(f'#{case}')
     for lst in field:
         print(*lst)
