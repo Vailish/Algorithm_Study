9eee506304599a85ea56ce7acf39ec61b8293aad
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-02-21 10:03:15 +0900

    [Silver III] Title: 바이러스, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,32 @@
+# boj_2606_바이러스
+# https://www.acmicpc.net/problem/2606
+
+# dfs
 def dfs(v):
-    visited[v] = 1
+    global cnt
+    cnt += 1
+    visited[v] = True
 
     for next_v in graph[v]:
-        if not visited[next_v]:
-            global total
-            total += 1
+        if visited[next_v] == 0:
             dfs(next_v)
 
 
-n = int(input())  # n = 정점
-m = int(input())  # m = 간선 개수
-graph = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
-total = 0
+# 정점의 수
+v = int(input())
+# 간선의 수
+n = int(input())
+# 방문 리스트
+visited = [0 for _ in range(v+1)]
 
-for _ in range(m):  # edges
+# 그래프 채우기
+graph = [[] for _ in range(v+1)]  # 0은 버림
+for _ in range(n):
     v1, v2 = map(int, input().split())
     graph[v1].append(v2)
     graph[v2].append(v1)
 
+cnt = 0
+
 dfs(1)
-print(total)
+print(cnt-1)  # 출발점은 제외해야 하기 때문
