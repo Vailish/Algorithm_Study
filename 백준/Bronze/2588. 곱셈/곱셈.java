0e51282fffdb42ce6fe8c222443817b82d31d658
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-09-16 13:15:22 +0900

    [Bronze III] Title: 곱셈, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int numA = Integer.parseInt(br.readLine());
+        String numB = br.readLine();
+
+        StringBuilder answer = new StringBuilder();
+        char[] tmp = numB.toCharArray();
+        answer.append(numA * (tmp[2]-'0'));
+        answer.append('\n');
+        answer.append(numA * (tmp[1]-'0'));
+        answer.append('\n');
+        answer.append(numA * (tmp[0]-'0'));
+        answer.append('\n');
+        answer.append(numA * Integer.parseInt(numB));
+        System.out.println(answer);
+    }
+}
