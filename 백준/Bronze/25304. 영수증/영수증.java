e00514dd2a634880bd2b6c7fb79880c6153f8bb0
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-09-16 16:35:11 +0900

    [Bronze IV] Title: 영수증, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int totalPrice = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int price = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            price += Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken());
+        }
+
+        if (totalPrice==price){
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
