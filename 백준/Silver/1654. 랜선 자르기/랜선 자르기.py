01aa1c865a2f61f488d99c2c8d4b395cda47eb79
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-09-30 17:44:05 +0900

    [Silver II] Title: 랜선 자르기, Time: 436 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,27 @@
+def solution():
+    K, N = map(int, input().split())
+    cables = []
+
+    for _ in range(K):
+        cables.append(int(input()))
+    start_point = 1
+    end_point = max(cables)
+
+    while start_point <= end_point:
+        middle_point = (start_point + end_point) // 2
+        cnt = 0
+
+        for cable in cables:
+            cnt += cable//middle_point
+
+        if cnt >= N:
+            start_point = middle_point + 1
+        else:
+            end_point = middle_point - 1
+
+    print(end_point)
+
+    return
+
+
+solution()
