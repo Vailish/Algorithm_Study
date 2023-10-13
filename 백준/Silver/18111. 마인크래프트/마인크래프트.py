637b006cc33015c1e21ad142e4f9e79e5405f7c0
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-13 18:17:38 +0900

    [Silver II] Title: 마인크래프트, Time: 856 ms, Memory: 112524 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,59 @@
+import sys
+from math import inf
+
+
+input = sys.stdin.readline
+
+
+def solution():
+    N, M, B = map(int, input().split())
+    field = []
+    max_height = 0
+    min_height = inf
+
+    # 필드와 높이 범위 구하기
+    for _ in range(N):
+        lst = list(map(int, input().split()))
+        field.append(lst)
+        if max(lst) > max_height:
+            max_height = max(lst)
+        if min(lst) < min_height:
+            min_height = min(lst)
+
+    result = -1
+    times = inf
+
+    # 목표 높이마다 확인하기
+    for target_height in range(max_height, min_height -1, -1):
+        cnt = 0
+        blocks = B
+
+        # 목표 높이마다 걸리는 시간과 가능성 확인하기
+        for i in range(N):
+            for j in range(M):
+
+                # 목표 높이 이상일 경우에 도달한 경우
+                # 블럭을 파서 가방에 넣기
+                if field[i][j] >= target_height:
+                    cnt += (field[i][j] - target_height) * 2
+                    blocks += (field[i][j] - target_height)
+
+                # 목표 높이보다 작을 경우
+                else:
+                    # 해당 블럭만큼 빼서 채워넣어줌
+                    cnt += (target_height - field[i][j])
+                    blocks -= (target_height - field[i][j])
+
+        # 사용한 블럭이 음수인 경우 패스
+        if blocks < 0:
+            continue
+
+        # 결과값을 비교
+        if times > cnt:
+            times = cnt
+            result = target_height
+
+    print(times, result)
+
+
+solution()
