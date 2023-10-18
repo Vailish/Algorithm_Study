507583fe480eed89f82dde4fcef597d9dca391f0
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-19 00:12:11 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 224 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder answer = new StringBuilder();
+        for (int i=1;i<n+1;i++) {
+            int spaceNum = n-i;
+            int starNum = 2*i - 1;
+            StringBuilder space = new StringBuilder();
+            for (int a=0;a<spaceNum;a++) {
+                space.append(" ");
+            }
+            StringBuilder star = new StringBuilder();
+            for (int b=0;b<starNum;b++) {
+                star.append("*");
+            }
+
+            answer.append(space).append(star).append("\n");
+        }
+
+        for (int i=1;i<n;i++) {
+            int spaceNum = i;
+            int starNum = 2*(n-i) - 1;
+            StringBuilder space = new StringBuilder();
+            for (int a=0;a<spaceNum;a++) {
+                space.append(" ");
+            }
+            StringBuilder star = new StringBuilder();
+            for (int b=0;b<starNum;b++) {
+                star.append("*");
+            }
+
+            answer.append(space).append(star).append("\n");
+        }
+
+        System.out.println(answer);
+    }
+}
