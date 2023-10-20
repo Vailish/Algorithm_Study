41529fd0232f1b674a4ebf478ff91a1184e8907c
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-20 23:48:40 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] chk = {1, 1, 2, 2, 2, 8};
+
+        StringBuilder answer = new StringBuilder();
+        for (int i=0; i<6; i++) {
+
+            int n = sc.nextInt();
+            answer.append(chk[i] - n).append(" ");
+        }
+
+        System.out.println(answer);
+
+    }
+}
