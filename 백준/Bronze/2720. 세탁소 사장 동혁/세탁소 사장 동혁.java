94440394b05890e34d0c370987d839366309d3ab
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-27 15:51:00 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 268 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] coins = {25, 10, 5, 1};
+        int len = sc.nextInt();
+        for (int i=0; i<len; i++) {
+            StringBuilder answer = new StringBuilder();
+            int value = sc.nextInt();
+            int remain;
+            for (int n : coins) {
+                remain = value % n;
+                answer.append(value / n).append(" ");
+                value = remain;
+            }
+
+            System.out.println(answer);
+        }
+    }
+}
