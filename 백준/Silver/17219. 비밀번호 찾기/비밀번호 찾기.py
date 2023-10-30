613da4773432c6616b14da6980b0d44689e79925
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-31 01:05:31 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 11916 ms, Memory: 49208 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,12 @@
+def solution():
+    my_dict = dict()
+    N, M = map(int, input().split())
+    for _ in range(N):
+        address, password = input().split()
+        my_dict[address] = password
+
+    for _ in range(M):
+        print(my_dict[input()])
+
+
+solution()
