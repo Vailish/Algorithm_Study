0b80894a806ef02726d2ed24ab77583ce31601ac
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-31 01:47:05 +0900

    [Silver IV] Title: 듣보잡, Time: 228 ms, Memory: 121524 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,19 @@
+def solution():
+    N, M = map(int, input().split())
+    my_dict = dict()
+    answer = []
+
+    for _ in range(N):
+        my_dict[input().strip()] = 1
+
+    for _ in range(M):
+        target = input().strip()
+        if target in my_dict:
+            answer.append(target)
+    answer.sort()
+    print(len(answer))
+    for value in answer:
+        print(value)
+
+
+solution()
