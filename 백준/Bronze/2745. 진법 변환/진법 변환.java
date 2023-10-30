3d4f3aed6feb9d93747f587aaa4c66a4d88d899f
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-31 00:34:03 +0900

    [Bronze II] Title: 진법 변환, Time: 220 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] rawNums = sc.next().split("");
+
+        int n = sc.nextInt();
+        int answer = 0;
+        int val = 1;
+
+        for (int i=rawNums.length-1; i>-1; i--) {
+            if (rawNums[i].charAt(0)<58) {
+                answer += (rawNums[i].charAt(0)-48) * val;
+            } else {
+                answer += (rawNums[i].charAt(0) - 55) * val;
+            }
+            val *= n;
+        }
+
+        System.out.println(answer);
+    }
+}
