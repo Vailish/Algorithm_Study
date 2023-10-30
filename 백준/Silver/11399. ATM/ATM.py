d69f76ca141eced4ad300900c5349f148a1e7f2d
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-10-31 01:18:53 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,13 @@
+def solution():
+    N = int(input())
+    lst = list(map(int, input().split()))
+    lst.sort()
+    answer = 0
+
+    for idx, value in enumerate(lst):
+        answer += (N-idx) * value
+
+    print(answer)
+
+
+solution()
