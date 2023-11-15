bb204f9aa96b8fa112239604a3ae6a54f06b98be
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-11-15 09:26:05 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 208 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int[] v = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+            Arrays.sort(v);
+            if (v[0]==0 && v[1]==0 && v[2]==0) {
+                return;
+            } else if (v[0]==v[1] && v[1]==v[2] && v[0]==v[2]) {
+                System.out.println("Equilateral");
+            } else if (v[2] < v[0] + v[1] && v[1] < v[0] + v[2] && v[0] < v[1] + v[2]) {
+                if (v[0]==v[1] || v[1]==v[2] || v[0]==v[2]) {
+                    System.out.println("Isosceles");
+                } else {
+                    System.out.println("Scalene");
+                }
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
