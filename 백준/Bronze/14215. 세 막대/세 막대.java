e42f276d8de3289444a684d42d917e973c9fe282
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-11-16 15:53:05 +0900

    [Bronze III] Title: 세 막대, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> nums = new ArrayList<>(List.of(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+        nums.sort(Comparator.naturalOrder());
+        int a = nums.get(0);
+        int b = nums.get(1);
+        int c = nums.get(2);
+
+        if (c >= a + b) {
+            System.out.println((a+b) * 2 - 1);
+        } else {
+            System.out.println(a+b+c);
+        }
+    }
+}
+
