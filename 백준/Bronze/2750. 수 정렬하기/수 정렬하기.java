7762ffd162c61c4dd555426e282bea16ad7ee83e
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-11-16 16:40:54 +0900

    [Bronze II] Title: 수 정렬하기, Time: 348 ms, Memory: 22128 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> nums = new ArrayList<>();
+        for (int i=0;i<n;i++) {
+            nums.add(sc.nextInt());
+        }
+        nums.sort(Comparator.naturalOrder());
+        for (int i=0; i<n; i++) {
+            System.out.println(nums.get(i));
+        }
+    }
+}
