0508a0f27ccd55d5e12e79e32bec0579188d1d51
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-11-21 18:47:24 +0900

    [Silver V] Title: 숫자 카드, Time: 2200 ms, Memory: 360336 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nLst = new int[n];
+        HashMap<Integer, Integer> nMap = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            nLst[i] = sc.nextInt();
+            nMap.put(nLst[i], 1);
+        }
+        StringBuilder answer = new StringBuilder();
+        int m = sc.nextInt();
+        for (int i=0; i<m; i++) {
+            int chk = sc.nextInt();
+            if (nMap.containsKey(chk)) {
+                answer.append(1).append(" ");
+            } else {answer.append(0).append(" ");}
+        }
+        System.out.println(answer);
+    }
+}
