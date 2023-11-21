d5314228a5929a49e158b04f585bbe0a93ad9934
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-11-21 20:04:43 +0900

    [Silver IV] Title: 문자열 집합, Time: 1080 ms, Memory: 68208 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        HashMap<String, Boolean> myDict = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            myDict.put(sc.next(), true);
+        }
+        int cnt = 0;
+
+        for (int i=0; i<m; i++) {
+            if (myDict.containsKey(sc.next())) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
