0feec5e0987c153158c22404628cb3ca6f674f57
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-11-21 19:57:20 +0900

    [Silver IV] Title: 대칭 차집합, Time: 1540 ms, Memory: 319792 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        HashMap<Integer, Boolean> mapN = new HashMap<>();
+        for (int i=0; i<n; i++) {
+            mapN.put(sc.nextInt(), true);
+        }
+        int cnt = 0;
+        for (int i=0; i<m; i++) {
+            if (mapN.containsKey(sc.nextInt())) {
+                cnt++;
+            }
+        }
+        System.out.println(n+m-cnt*2);
+    }
+}
