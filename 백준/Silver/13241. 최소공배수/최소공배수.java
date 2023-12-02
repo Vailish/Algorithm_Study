b94716364625c7f7ebae803adb06b4c1e72dffa9
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-12-02 16:43:50 +0900

    [Silver V] Title: 최소공배수, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static long gcd(long a, long b) {
+        long r;
+        while (b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        System.out.println(a * b / gcd(a, b));
+    }
+}
