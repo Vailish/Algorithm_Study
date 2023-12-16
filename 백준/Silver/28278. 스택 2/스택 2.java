86d2778a806ddcf12364e3be3b0de7279faed7d9
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2023-12-16 10:42:44 +0900

    [Silver IV] Title: 스택 2, Time: 1664 ms, Memory: 325956 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+
+public interface Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder answer = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] inputs;
+        for (int i=0; i<n; i++) {
+            inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            switch (inputs[0]) {
+                case 1:
+                    stack.push(inputs[1]); break;
+                case 2:
+                    if (stack.empty()) answer.append(-1).append("\n");
+                    else answer.append(stack.pop()).append("\n"); break;
+                case 3:
+                    answer.append(stack.size()).append("\n"); break;
+                case 4:
+                    if (stack.empty()) answer.append(1).append("\n");
+                    else answer.append(0).append("\n"); break;
+                case 5:
+                    if (!stack.empty()) answer.append(stack.peek()).append("\n");
+                    else answer.append(-1).append("\n");
+            }
+        }
+        System.out.println(answer);
+    }
+}
