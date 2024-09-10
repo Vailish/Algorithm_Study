2113fcfc4a6a232b5e00eaba11e7df5e45f57ebd
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2024-09-10 09:01:54 +0900

    [Silver V] Title: 뒤집기, Time: 100 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -9,7 +9,6 @@ public class Main {
 
         // 0 -> 0으로 변환 횟수, 1 -> 1으로 변환 횟수
         int[] result = new int[2];
-        boolean isChange = false;
 
         char exNum = nums.charAt(0);
         char nowNum;
@@ -23,20 +22,8 @@ public class Main {
                 exNum = nowNum;
                 // result 채워넣기
                 result[exNum - '0']++;
-                isChange = true;
             }
         }
-        if (!isChange) {
-            System.out.println(0); return;
-        }
-        int answer;
-        if (result[0]==0) {
-            answer = result[1];
-        } else if (result[1]==0) {
-            answer = result[0];
-        } else {
-            answer = Math.min(result[0], result[1]);
-        }
-        System.out.println(answer);
+        System.out.println(Math.min(result[0], result[1]));
     }
 }
