6ff5c2a7e1176f8fb480c9ee24ae9b2538cc0a3b
Seong-jun Kim <109258380+Vailish@users.noreply.github.com>
2025-03-14 08:51:07 +0900

    [level 0] Title: 할 일 목록, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> result = new ArrayList<>();
+        for (int i=0; i<todo_list.length; i++) {
+            if (!finished[i]) {
+                result.add(todo_list[i]);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
